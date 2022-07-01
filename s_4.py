6a9377d1c0790f47cddbcd8915aab4bff3ff208b
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-01 12:31:40 +0530

    Add files via upload

---
+++ s_4.py
@@ -0,0 +1,42 @@
+# e={}
+# print(e,type(e))  {} <class 'dict'>
+# t={3,6,8,9}
+# f=frozenset(t)
+# print(t)     {8, 9, 3, 6}
+# m={'hello',98,678}
+# print(m){98, 'hello', 678}
+# h={'',546,2,'wtr'}
+# print(h.clear())None
+# y={"hello",'thyuiom',34,980}
+# print(y.copy())  {'hello', 'thyuiom', 980, 34}
+# y={34,"ehimnbvgtr",467}
+# print(y.discard(34))
+# m={34,(1,2,3)}
+# print(m.issubset(m))    True
+# e={'poiuy',980,'sedfion'}
+# print(e.issubset(e))    True
+# j={78,987,45}
+# j.pop()
+# print(j)   {45, 78}
+# w={34,45,56,78}
+# w.add(98)
+# print(w)    {34, 98, 45, 78, 56}
+# l={34,4,56,45}
+# l.remove(4)
+# print(l)    {56, 34, 45}
+# a={'number','int',"float"}
+# u={'hi','derty','ruwbgiwo'}
+# h={'srftyhiol'}
+# print(u.union(h))     {'srftyhiol', 'derty', 'ruwbgiwo', 'hi'}
+# g={34,5,6,78}
+# b={4,34,56}
+# g.update(b)
+# print(g)      {34, 4, 5, 6, 56, 78}
+# m={'hjioentu','sedrtf'}
+# n={'wsqaed','imjukopln'}
+# m.update(n)
+# print(m)     {'imjukopln', 'sedrtf', 'wsqaed', 'hjioentu'}
+# t={'rhfgty','nimh','sgdtre'}
+# s={'sdewaqcv'}
+# print(t.isdisjoint(s))   True
+ 
